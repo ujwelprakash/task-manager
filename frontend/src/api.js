3050ed3d011f590de2ef24c5994949be45e1d189
ujwelprakash <ujwelprakash@gmail.com>
2025-09-15 00:00:31 +0530

    Fix CORS issue for frontend and update API config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-backend.onrender.com/api",
+
   headers: {
     "Content-Type": "application/json", // 👈 Force JSON
   },
